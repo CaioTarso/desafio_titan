2af94c171055c11a185de2ee9d8b7e3f6742092b
caiotarso <caiotarso7@gmail.com>
2026-08-31 00:35:50 -0300

    style: estilizando view de dashboard com tabela de serviços + criação da tela de criação de serviços

---
+++ app/views/createservice.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Criar Serviço</title>
+</head>
+<body>
+    <h2>Criar Serviço</h2>
+
+    <div class="info">
+            
+            <form action="/" method="POST">
+
+                <input type="hidden" name="action" value="createservice">
+
+                <div class="form-container">
+
+                    <?php if (isset($error)): ?>
+                        <p><?= $error ?></p>
+                    <?php endif; ?>
+
+                    <div class="form-group">
+
+                        <label for="description">Descrição:</label>
+
+                        <input
+                            type="text"
+                            id="description"
+                            name="description"
+                            placeholder="Descrição do serviço"
+                            required
+                        >
+
+                    </div>
+
+                    <div class="form-group">
+
+                        <label for="price">Valor:</label>
+
+                        <input
+                            type="number"
+                            id="price"
+                            name="price"
+                            placeholder="Insira o valor"
+                            step="0.01"
+                            required
+                        >
+
+                    </div>
+
+                    <div class="bottom">
+
+                        <button type="submit">
+                            Cadastrar
+                        </button>
+
+                    </div>
+
+                </div>
+
+        </form>
+    </div>
+</body>
+</html>
--- app/views/dashboard.php
+++ app/views/dashboard.php
@@ -1,22 +1,66 @@
-<?php
-
-
-?>
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/global.css">
+    <link rel="stylesheet" href="assets/css/dashboard.css">
+    <title>dashboard</title>
 </head>
 <body>
     <header>
         <h1>JM Informática</h1>
-        <button><a href="/?page=logout">Sair</a></button>
+        <h3>Olá, <?= $_SESSION['user']['name'] ?>!</h3>
+        <a href="/?page=logout">Sair</a>
     </header>
-    <h2>Bem-vindo ao Dashboard</h2>
+
+    <div class="container">
+        <div class="dash-elements">
+            <h2>Dashboard</h2>
+
+            <a class="button-create"  href="/?page=createservice">Criar serviço</a>
+
+
+        </div>
+
+
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Descrição</th>
+                    <th>Preço</th>
+                    <th>Usuário</th>
+                  
+                    <th>Status</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+
+        <tbody>
+
+            <?php foreach ($services as $service): ?>
+
+                <tr>
+                    <td><?= $service['id_service'] ?></td>
+                    <td><?= $service['description'] ?></td>
+                    <td>R$ <?= $service['price'] ?></td>
+                    <td><?= $service['name'] ?></td>
+
+                    <td>
+                        <?php if ($service['finished_at']): ?>
+                            Finalizado
+                        <?php else: ?>
+                            Pendente
+                        <?php endif; ?>
+                    </td>
+
+                </tr>
+
+            <?php endforeach; ?>
+
+        </tbody>
+    </table>
+</div>
 </body>
 </html>
